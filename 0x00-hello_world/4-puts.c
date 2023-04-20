7f6db1eb0305176ce9bc76f686cd9e7651096161
ireneMugure <irenemugurekihiu@gmail.com>
2023-04-20 08:37:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,11 @@
  * followed by a newline.
  * Return: Always 0  
  */
+
 int main(void)
+
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
+
 }
