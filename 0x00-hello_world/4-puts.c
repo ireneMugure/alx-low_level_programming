812856396ba758e34fe809745a0284fb19592962
ireneMugure <irenemugurekihiu@gmail.com>
2023-04-20 08:31:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle,
- * followed by a newline."
- * Return 0 
+ * followed by a newline.
+ * Return: Always 0  
  */
 int main(void)
 {
    puts("\"Programming is like building a multilingual puzzle");
 
-   return(0);
+   return (0);
 }
