16e1fb6b77f84dc5aed0f3747dcb031a2e8ed8b6
ireneMugure <irenemugurekihiu@gmail.com>
2023-04-27 07:07:53 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Print numbers from 0 to 9
+ * Return: The numbers 0 to 9
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
