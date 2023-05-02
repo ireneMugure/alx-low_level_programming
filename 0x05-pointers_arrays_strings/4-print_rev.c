8a2cd4e3b4af628c9aea000192a7e316b26e9c9a
ireneMugure <irenemugurekihiu@gmail.com>
2023-05-02 09:02:06 -0700

    ./4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* Print_rev - it prints a string in rev to the stdout
+* @s: The string
+* Return: Nothing
+*/
+void print_rev(char *S)
+{
+	int c = 0;
+
+	while (S[c] != '\0')
+	{
+	c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+	_putchar(S[c]);
+	}
+	_putchar('\n');
+}
