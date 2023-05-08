6d504b521a586d6ba5defa578184738799b2b647
ireneMugure <irenemugurekihiu@gmail.com>
2023-05-08 05:04:54 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include " main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input value
+ * @needle: input value
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+
+		char *i = haystack;
+		char *j = needle;
+
+		while (*i == *j && *j != '\0')
+		{
+			i++;
+			j++;
+		}
+
+		if (*j == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
