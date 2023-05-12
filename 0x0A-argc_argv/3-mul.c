b426a4d84be6d20fbab4204bf75d7fe44d0ee2b1
ireneMugure <irenemugurekihiu@gmail.com>
2023-05-12 11:56:40 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,13 +5,10 @@
 * @s: string
 * Return: the int converted from the string
 */
-
 int _atoi(char *s)
 {
 
 	int i, d, n, len, f, digit;
-
-
 
 	i = 0;
 	d = 0;
@@ -28,10 +25,8 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-
 			digit = s[i] - '0';
 			if (d % 2)
 
@@ -41,48 +36,37 @@ int _atoi(char *s)
 			f = 1;
 
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-
 				break;
 			f = 0;
 		}
 
 		i++;
-
 	}
-
 	if (f == 0)
 
 		return (0);
 
 	return (n);
-
 }
-
-
 /**
  * main - multiplies two nums
  * @argc: number of args
  * @argv: array of args
  * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
-
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-
 	printf("%d\n", result);
 
 	return (0);
-
 }
