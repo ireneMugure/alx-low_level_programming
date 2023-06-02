f2ba618028069a63fa2e8137bb97dd83cd82f1b7
ireneMugure <irenemugurekihiu@gmail.com>
2023-06-02 09:08:31 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,10 @@
 global main
+extern printf
 main:
-    ; Push the format string and the hello message onto the stack
-    push format
-    push hello
 
-    ; Call printf
-    mov rdi, rsp
-    xor eax, eax
-    call printf
-	mov eax,0
-	ret
-	format: db 'Hello, Holberton\n',0
+mov edi, format
+xor eax, eax
+call print
+mov eax,0
+ret
+format: db 'Hello, Holberton\n',0
