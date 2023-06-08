df7fc38cd1538096cd44470bb7b0128133e91b0d
ireneMugure <irenemugurekihiu@gmail.com>
2023-06-08 05:21:50 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-
+	int i;
 	unsigned int result = 0;
-	int i = 0;
 
-	while (b[i] != '\0')
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		result = result * 2 + (b[i] != '0');
-		i++;
+		result = 2 * result + (b[i] - '0');
 	}
+
 	return (result);
 }
